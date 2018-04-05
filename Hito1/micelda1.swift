840e898f8413bbed1149ba9b463af25bec2c0521
AlvaroBarrado <barradoa95@gmail.com>
2018-04-05 10:28:57 +0200

    Nuevo Hito1 cambiado

---
+++ Hito1/micelda1.swift
@@ -0,0 +1,24 @@
+//
+//  micelda1.swift
+//  Hito1
+//
+//  Created by Alvaro Barrado on 5/4/18.
+//  Copyright © 2018 Alvaro Barrado. All rights reserved.
+//
+
+import UIKit
+
+class micelda1: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
