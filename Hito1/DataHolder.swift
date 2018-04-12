5670558c8c6a77f5b20b729affed047edc981154
AlvaroBarrado <barradoa95@gmail.com>
2018-04-12 10:39:19 +0200

    Kiran y Alvaro Barrado

---
+++ Hito1/DataHolder.swift
@@ -0,0 +1,13 @@
+//
+//  DataHolder.swift
+//  Hito1
+//
+//  Created by Alvaro Barrado on 10/4/18.
+//  Copyright © 2018 Alvaro Barrado. All rights reserved.
+//
+
+import UIKit
+
+class DataHolder: NSObject {
+
+}
